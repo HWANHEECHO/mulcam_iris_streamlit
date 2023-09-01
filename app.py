574af2525a30c55374d7bebcb63ba739da28ab03
CHO HWANHEE <chemical0128@gmail.com>
2023-09-01 09:53:38 +0900

    streamlit_practice_01

--- app.py
+++ app.py
@@ -2,10 +2,21 @@
 
 import streamlit as st
 import numpy as np
+import pandas as pd
+import matplotlib
+import seaborn as sns
+import scikit-learn as sk
+import plotly as go
 
 def main():
 
     st.markdown('# Hello World')
+    st.write(np.__version__)
+    st.write(pd.__version__)
+    st.write(matplotlib.__version__)
+    st.write(sns.__version__)
+    st.write(sk.__version__)
+    st.write(go.__version__)
     st.write(np.__version__)
 
 if __name__ == '__main__':
