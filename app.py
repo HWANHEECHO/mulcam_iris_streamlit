713eeab36f6beee20049e646a3e747f11e123f96
CHO HWANHEE <chemical0128@gmail.com>
2023-09-01 10:23:05 +0900

    streamlit_practice_03

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 # -*- coding:utf-8 -*-
 
 import streamlit as st
-import numpy as np
-import pandas as pd
 
 def main():
 
     st.markdown('# Hello World')
-    st.write(np.__version__)
-    st.write(pd.__version__)
+
+    menu = ['Home', '탐색적 자료 분석', '머신러닝', 'About']
+    choice = st.sidebar.selectbox('메뉴', menu)
+
+    if choice == 'Home':
+        st.subheader('Home')
+    elif choice == '탐색적 자료 분석':
+        st.subheader('탐색적 자료 분석')
+    elif choice == '머신러닝':
+        st.subheader('머신러닝')
+    elif choice == 'About':
+        st.subheader('About')
+    else:
+        pass
 
 if __name__ == '__main__':
     main()
